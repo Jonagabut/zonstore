8ab8cc2df96ad929a60b795e4eca9f9dec392f44
Jonagabut <108325696+Jonagabut@users.noreply.github.com>
2023-12-27 21:51:16 +0700

    ml.js

--- ml.js
+++ ml.js
@@ -19,8 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
         { name: "514 DM", description: "", price: 131.00, image: "ml.png" },
         { name: "600 DM", description: "", price: 151.00, image: "ml.png" },
         { name: "706 DM", description: "", price: 178.20, image: "ml.png" },
-        { name: "WDP", description: "Weekly diamond pass", price: 28.5, image: "wdp.png" },
-        { name: "WDP 2x", description: "Weekly diamond pass", price: 55.4, image: "wdp.png" },
+        { name: "WDP", description: "Weekly diamond pass", 
+price: 28.500, image: "wdp.png" },
+        { name: "WDP 2x", description: "Weekly diamond pass", 
+price: 55.400, image: "wdp.png" },
+        
         // Tambahkan produk lainnya sesuai kebutuhan
     ];
 
@@ -69,65 +72,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function formatRupiah(amount) {
-        return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount * 1000);
-    }
-
-    function generateOrderId() {
-        return Date.now().toString() + Math.random().toString(36).substring(2, 15);
+        return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
     }
 
     window.addToCart = addToCart;
     window.cancelOrder = cancelOrder;
     window.checkout = function () {
         if (cart.length > 0) {
+            // Implementasi logika pembayaran di sini (contoh sederhana hanya mengonfirmasi)
             var confirmation = confirm(`Total belanja Anda: ${formatRupiah(total)}\nApakah Anda ingin melanjutkan pembayaran?`);
 
             if (confirmation) {
-                var orderId = generateOrderId();
-                var midtransClientKey = 'Mid-client-G3QKXRp2Ff0dySnF';  // Gantilah dengan kunci Midtrans yang valid
+                var customerData = prompt("Masukan ID(SERVER ):");
 
-                var midtransParams = {
-                    transaction_details: {
-                        order_id: orderId,
-                        gross_amount: total * 1000,
-                    },
-                    credit_card: {
-                        secure: true,
-                    },
-                };
+                if (customerData) {
+                    var message = `Pembelian dari zon store:\n\n${cart.map(item => `${item.name} - ${formatRupiah(item.price)}`).join('\n')}\n\nTotal: ${formatRupiah(total)}\n\nPembeli: ${customerData}`;
 
-                snap.pay(midtransParams, {
-                    onSuccess: function (result) {
-                        var customerData = prompt("Masukkan ID ML:");
-
-                        if (customerData) {
-                            var message = `Pembelian dari Zon Store:\n\n${cart.map(item => `${item.name} - ${formatRupiah(item.price)}`).join('\n')}\n\nTotal: ${formatRupiah(total)}\n\nPembeli: ${customerData}`;
-
-                            window.location.href = `https://wa.me/6285173138301?text=${encodeURIComponent(message)}`;
-                            alert("Pesanan Anda telah berhasil. Terima kasih!");
-                            resetCart();
-                        }
-                    },
-                    onPending: function (result) {
-                        alert("Pembayaran sedang diproses. Terima kasih!");
-                        resetCart();
-                    },
-                    onError: function (result) {
-                        alert("Terjadi kesalahan saat memproses pembayaran. Silakan coba lagi.");
-                    },
-                    onClose: function () {
-                        // Callback ketika widget Midtrans tertutup
-                    },
-                });
+                    window.location.href = `https://wa.me/6285173138301?text=${encodeURIComponent(message)}`;
+                    // Setelah mengirim data ke WhatsApp, Anda dapat mereset keranjang belanja atau melakukan langkah lainnya sesuai kebutuhan
+                    alert("Pesanan Anda telah berhasil. Terima kasih!");
+                }
             }
         } else {
             alert("Keranjang belanja Anda kosong. Silakan tambahkan produk terlebih dahulu.");
         }
     };
-
-    function resetCart() {
-        cart = [];
-        total = 0;
-        updateCart();
-    }
 });
+
+
