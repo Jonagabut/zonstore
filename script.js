874d34c4eeb66cea25b558a9ce66347a29dfc632
Jonagabut <108325696+Jonagabut@users.noreply.github.com>
2024-01-03 08:26:21 +0700

    script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (confirmation) {
                     var message = `Pembelian dari zon store:\n\n${cart.map(item => `${item.name} - ${formatRupiah(item.price)}`).join('\n')}\n\nTotal: ${formatRupiah(total)}\n\nPelanggan: ${customerName}\nID Pelanggan: ${customerId}`;
 
-                    window.location.href = `https://wa.me/6285173138301?text=${encodeURIComponent(message)}`;
+                    window.location.href = `https://wa.me/6285176708301?text=${encodeURIComponent(message)}`;
                     alert("Pesanan Anda telah berhasil. Terima kasih!");
                 }
             }
